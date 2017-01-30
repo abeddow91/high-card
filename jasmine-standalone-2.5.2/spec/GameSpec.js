a90450051ee1235bd0990f5783a8fd694ec73a2f
Anna Beddow <anna.beddow@gmail.com>
2017-01-30 23:41:02 +0000

    added player initialize tests;

--- jasmine-standalone-2.5.2/spec/GameSpec.js
+++ jasmine-standalone-2.5.2/spec/GameSpec.js
@@ -14,4 +14,16 @@ describe ("Game", function() {
                  "2s","3s","4s","5s","6s","7s","8s","9s","10s","Js","Qs","Ks","As",
                  "2d","3d","4d","5d","6d","7d","8d","9d","10d","Jd","Qd","Kd","Ad",])
   });
+  it("should initialize the game with a player one who has no cards", function() {
+    expect(game.playerOne).toEqual([]);
+  });
+  it("should initialize the game with a player one who has no cards", function() {
+    expect(game.playerTwo).toEqual([]);
+  });
+  it("should initialize the game with a player one who has no cards", function() {
+    expect(game.playerThree).toEqual([]);
+  });
+  it("should initialize the game with a player one who has no cards", function() {
+    expect(game.playerFour).toEqual([]);
+  });
 });
