9c0fff7f5db9a36ea39ad0631a6d99bd51f8f8ef
Anna Beddow <anna.beddow@gmail.com>
2017-01-30 23:48:06 +0000

    added git shuffle test

--- jasmine-standalone-2.5.2/spec/GameSpec.js
+++ jasmine-standalone-2.5.2/spec/GameSpec.js
@@ -1,5 +1,9 @@
 describe ("Game", function() {
   var game;
+  var perfectDeck = ["2h","3h","4h","5h","6h","7h","8h","9h","10h","Jh","Qh","Kh","Ah",
+               "2c","3c","4c","5c","6c","7c","8c","9c","10c","Jc","Qc","Kc","Ac",
+               "2s","3s","4s","5s","6s","7s","8s","9s","10s","Js","Qs","Ks","As",
+               "2d","3d","4d","5d","6d","7d","8d","9d","10d","Jd","Qd","Kd","Ad"]
 
   beforeEach(function() {
     game = new Game();
@@ -9,21 +13,22 @@ describe ("Game", function() {
     expect(game.deck.length).toBe(52);
   })
   it("should have a deck in perfect sequence", function() {
-    expect(game.deck).toEqual(["2h","3h","4h","5h","6h","7h","8h","9h","10h","Jh","Qh","Kh","Ah",
-                 "2c","3c","4c","5c","6c","7c","8c","9c","10c","Jc","Qc","Kc","Ac",
-                 "2s","3s","4s","5s","6s","7s","8s","9s","10s","Js","Qs","Ks","As",
-                 "2d","3d","4d","5d","6d","7d","8d","9d","10d","Jd","Qd","Kd","Ad",])
+    expect(game.deck).toEqual(perfectDeck);
   });
-  it("should initialize the game with a player one who has no cards", function() {
+  it("should initialize the game with player one who has no cards", function() {
     expect(game.playerOne).toEqual([]);
   });
-  it("should initialize the game with a player one who has no cards", function() {
+  it("should initialize the game with player two who has no cards", function() {
     expect(game.playerTwo).toEqual([]);
   });
-  it("should initialize the game with a player one who has no cards", function() {
+  it("should initialize the game with player three who has no cards", function() {
     expect(game.playerThree).toEqual([]);
   });
-  it("should initialize the game with a player one who has no cards", function() {
+  it("should initialize the game with player four who has no cards", function() {
     expect(game.playerFour).toEqual([]);
+  });
+  it("should shuffle the cards out of perfect sequence", function () {
+    game.shuffle(this.deck);
+    expect(this.deck).not.toEqual(perfectDeck);
   });
 });
